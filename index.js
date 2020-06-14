bb215132a5bd3eb281c1765d32b148de60a8c369
WD200019 <warrend2000@gmail.com>
2020-06-14 16:14:22 -0500

    Updated to follow May migration guide
    Updated index.js to remove to-be-deprecated commands following this guide: https://github.com/powercord-org/powercord/wiki/May-2020-Migration-Guide

--- index.js
+++ index.js
@@ -4,72 +4,72 @@ const { resolve } = require("path");
 const Settings = require("./Settings");
 
 module.exports = class Alias extends Plugin {
-  async startPlugin () {
-    this.registerSettings(
-      "alias",
-      "Alias",
-      Settings
-    );
-    this.loadCSS(resolve(__dirname, "style.css"));
-    this.registerCommand(
-      "alias",
-      ["a"],
-      "Send a message you previously set in the \"Alias\" settings menu.",
-      "{c} [ alias name ]",
-      function (args) {
-        if (args.length == 0)
-          return {
-            send: false,
-            result: {
-              type: "rich",
-              title: "Alias",
-              description: "Please enter an alias name.\nYou have " + this.settings.get("pairs", []).length.toString() + " alias" + (this.settings.get("pairs", []).length == 1 ? "" : "es")
-                + (this.settings.get("pairs", []).length > 0 ? (":" + this.listAliases()) : "."),
-              footer: { text: `Usage: ${powercord.api.commands.prefix}alias <aliasName>` }
-            }
-          }
-        else {
-          let match = this.settings.get("pairs", []).filter(this.unique).find(p => p[0] == args[0]);
-          if (match != null)
-            return {
-              send: true,
-              result: match[1] + (args.length > 0 ? (" " + args.splice(1, args.length - 1).join(" ")) : "")
-            }
-          else
+  startPlugin() {
+    powercord.api.settings.registerSettings("alias", {
+        category: this.entityID,
+        label: "Alias",
+        render: Settings
+    });
+    this.loadStylesheet(resolve(__dirname, "style.css"));
+    powercord.api.commands.registerCommand({
+        command: "alias",
+        aliases: ["a"],
+        description: "Send a message you previously set in the \"Alias\" settings menu.",
+        usage: "{c} [ alias name ]",
+        executor: function (args) {
+          if (args.length == 0)
             return {
               send: false,
               result: {
                 type: "rich",
                 title: "Alias",
-                description: "The alias `" + args[0].toString() + "` was not found.",
+                description: "Please enter an alias name.\nYou have " + powercord.api.settings.store.getSetting("alias", "pairs", []).length.toString() + " alias" + (powercord.api.settings.store.getSetting("alias", "pairs", []).length == 1 ? "" : "es")
+                  + (powercord.api.settings.store.getSetting("alias", "pairs", []).length > 0 ? (":" + this.listAliases()) : "."),
                 footer: { text: `Usage: ${powercord.api.commands.prefix}alias <aliasName>` }
               }
             }
-        }
-      }.bind(this),
-      function (args) {
-        const output = this.settings.get("pairs", [])
-                         .filter(this.unique)
-                         .filter(p => p[0].toLowerCase().includes((args[0] || "").toLowerCase()) && ((args.length > 0 && p[0].toLowerCase() !== args[0].toLowerCase()) || args.length == 0))
-                         .map(function(currentValue, index, arr) {
-                           return {
+          else {
+            let match = powercord.api.settings.store.getSetting("alias", "pairs", []).filter(this.unique).find(p => p[0] == args[0]);
+            if (match != null)
+              return {
+                send: true,
+                result: match[1] + (args.length > 0 ? (" " + args.splice(1, args.length - 1).join(" ")) : "")
+              }
+            else
+              return {
+                send: false,
+                result: {
+                  type: "rich",
+                  title: "Alias",
+                  description: "The alias `" + args[0].toString() + "` was not found.",
+                  footer: { text: `Usage: ${powercord.api.commands.prefix}alias <aliasName>` }
+                }
+              }
+          }
+        }.bind(this),
+        autocomplete: function (args) {
+          let output = powercord.api.settings.store.getSetting("alias", "pairs", [])
+                           .filter(this.unique)
+                           .filter(p => p[0].toLowerCase().includes((args[0] || "").toLowerCase()) && ((args.length > 0 && p[0].toLowerCase() !== args[0].toLowerCase()) || args.length == 0))
+                           .map(function(currentValue, index, arr) {
+                             return {
                                command : currentValue[0],
                                aliases : [],
                                description : currentValue[1],
                                func : function() {},
                                autocompleteFunc : function() {}
-                           }
-                         });
-        return (output.length > 0) ? {
-          commands:  output,
-          header: "Aliases"
-        } : null
-      }.bind(this)
-    );
+                             }
+                           });
+          return (output.length > 0) ? {
+            commands:  output,
+            header: "Aliases"
+          } : null;
+        }.bind(this)
+    });
   }
 
-  unique(value, index, self) {
-    return self.filter(function(v, k, t) { return v[0] === value }).length <= 1;
+  unique(val, index, self) {
+    return self.filter(function(v, k, t) { return v[0] === val }).length <= 1;
   }
 
   listAliases() {
@@ -77,16 +77,16 @@ module.exports = class Alias extends Plugin {
     let spaceChar = " ";
 
     let longestName = 0;
-    for (let i = 0; i < (this.settings.get("pairs", []).length < 10 ? this.settings.get("pairs", []).length : 10); i++) {
-      let firstPart = powercord.api.commands.prefix + "alias " + this.settings.get("pairs", [])[i][0];
+    for (let i = 0; i < (powercord.api.settings.store.getSetting("alias", "pairs", []).length < 10 ? powercord.api.settings.store.getSetting("alias", "pairs", []).length : 10); i++) {
+      let firstPart = powercord.api.commands.prefix + "alias " + powercord.api.settings.store.getSetting("alias", "pairs", [])[i][0];
       longestName = firstPart.length > longestName ? firstPart.length : longestName;
     }
 
-    for (let i = 0; i < (this.settings.get("pairs", []).length < 10 ? this.settings.get("pairs", []).length : 10); i++) {
-      let firstPart = "\n" + powercord.api.commands.prefix + "alias " + this.settings.get("pairs", [])[i][0];
+    for (let i = 0; i < (powercord.api.settings.store.getSetting("alias", "pairs", []).length < 10 ? powercord.api.settings.store.getSetting("alias", "pairs", []).length : 10); i++) {
+      let firstPart = "\n" + powercord.api.commands.prefix + "alias " + powercord.api.settings.store.getSetting("alias", "pairs", [])[i][0];
       firstPart = firstPart + (spaceChar.repeat(longestName - (firstPart.length - 1))) + " : ";
 
-      let secondPart = this.settings.get("pairs", [])[i][1];
+      let secondPart = powercord.api.settings.store.getSetting("alias", "pairs", [])[i][1];
       if (firstPart.length + secondPart.length > 55) {
         secondPart = secondPart.substring(0, 55 - firstPart.length);
         secondPart += "...";
